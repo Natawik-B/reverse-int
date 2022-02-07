b8b2243fa6e4ab4f8206bc487eb5ac25abd97b31
Natawik-B <natawwwik37@gmail.com>
2022-02-07 21:35:38 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n < 0) {
+        const number = n*(-1);
+        let nString = number.toString();
+        let numAarr = [...nString];
+        let numTurn = numAarr.reverse();
+        let numJoin = numTurn.join("");
+        return (numJoin);
+    } else {
+        let nString = n.toString();
+        let numAarr = [...nString];
+        let numTurn = numAarr.reverse();
+        let numJoin = numTurn.join("");
+        return (numJoin);
+    }
+};
